9d5cd8894884c3869afbcaea9443097a4a6212a5
Rodrigo Paniagua <paniagua.camacho.rodrigo@gmail.com>
2025-11-10 23:56:25 -0400

    deploy

--- protocol/next.config.js
+++ protocol/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
+  // 👇 Esta línea le dice a Next dónde está tu código fuente
+  srcDir: 'src',
 };
 
 module.exports = nextConfig;
