275f7fdd552f985e9e9f5152f0bb72346cd08399
Rodrigo Paniagua <paniagua.camacho.rodrigo@gmail.com>
2025-11-11 01:08:33 -0400

    push

--- protocol/src/app/vestimenta/page.tsx
+++ protocol/src/app/vestimenta/page.tsx
@@ -18,9 +18,7 @@ export default function VestimentaPage() {
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <p className="text-gray-600 leading-relaxed">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            </p>
-          </div>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiumentore veritatis et 
         </section>
 
         <section id="equipos" className="mb-12">
