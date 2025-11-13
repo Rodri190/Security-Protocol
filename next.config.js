cc49dc7a67b56b252d181af41da28979971bfade
Oliver Pereira <oliver.pereira@jala.university>
2025-11-13 15:30:36 -0400

    eliminar configuraciones de salida y optimización de imágenes en next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/Security-Protocol' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Security-Protocol/' : '',
+
 };
 
 module.exports = nextConfig;
